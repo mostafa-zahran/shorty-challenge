da606ffe44a03ff2c5fcfe8b146392fb9e830001
Mostafa Zahran <mostafa.k.zahran@gmail.com>
2018-08-20 16:05:03 +0300

    add package file for easy require

---
+++ shorty_app/use_cases/shorty_use_cases.rb
@@ -0,0 +1,4 @@
+require_relative './shorty_use_cases/base.rb'
+require_relative './shorty_use_cases/create_shorty.rb'
+require_relative './shorty_use_cases/find_shorty_stats.rb'
+require_relative './shorty_use_cases/find_shorty_url.rb'
