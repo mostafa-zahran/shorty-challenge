70b74206d192804a54fd7d8fca1e7d3900009296
Mostafa Zahran <mostafa.k.zahran@gmail.com>
2018-08-20 16:06:07 +0300

    add initialization file for shorty app

---
+++ shorty_app/shorty_app.rb
@@ -0,0 +1,8 @@
+require_relative './shorty_exception.rb'
+require_relative './entities/shorty_entity.rb'
+require_relative './repository_interfaces/shorty_repository_interface.rb'
+require_relative './use_cases/shorty_use_cases.rb'
+
+# ShortyApp initialization
+module ShortyApp
+end
---
+++ spec/shorty_app/shorty_app_spec.rb
@@ -0,0 +1,8 @@
+require "spec_helper"
+
+describe ShortyApp do
+
+  it 'should be defined' do
+    expect(ShortyApp.class).to eq(Module)
+  end
+end
